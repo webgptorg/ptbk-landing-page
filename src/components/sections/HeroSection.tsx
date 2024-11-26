244ec98feea3b40dbd7faba3fd71db9679495556
Pavol Hejny <me@pavolhejny.com>
2024-11-26 18:52:08 +0100

    Design

--- src/components/sections/HeroSection.tsx
+++ src/components/sections/HeroSection.tsx
@@ -3,12 +3,12 @@ import { Button } from '@/components/ui/button';
 
 export function HeroSection() {
     return (
-        <div className="relative overflow-hidden">
+        <div className="relative overflow-hidden p-8">
             <BackgroundGrid />
             <div className="max-w-3xl space-y-8 relative z-10">
                 {/* added z-10 to put content above grid */}
                 <h1 className="scroll-m-20 text-6xl font-extrabold tracking-tight lg:text-7xl">
-                    Program in Plain English
+                    Programming in Plain English
                 </h1>
                 <p className="text-xl text-muted-foreground">
                     A revolutionary 4th generation language that transforms natural language specifications into
