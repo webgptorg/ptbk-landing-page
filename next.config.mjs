482a1eec8750f347353dfcb25de4564329265d9c
Pavol Hejny <me@pavolhejny.com>
2025-03-16 14:21:10 +0100

    Comment out the custom build ID generation in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    /*
     generateBuildId: async () => {
         return process.env.GIT_HASH;
         // <- Note: This could be anything, using the latest git hash
     },
+    */
     webpack(config) {
         config.module.rules.push({
             test: /\.svg$/,
