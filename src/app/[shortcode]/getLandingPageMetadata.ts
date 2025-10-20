bbac4ce47a08fcbcfa6ad8989f5dd7d6f365d7f8
Pavol Hejny <me@pavolhejny.com>
2025-10-20 10:42:06 +0200

    Shortened links with Landing pages should have nice Open Graph metadata for better sharing on social media - fixes & tweaks

--- src/app/[shortcode]/getLandingPageMetadata.ts
+++ src/app/[shortcode]/getLandingPageMetadata.ts
@@ -1,9 +1,14 @@
+import { removeMarkdownComments, removeMarkdownFormatting } from '@promptbook/markdown-utils';
+
 export function getLandingPageMetadata(landingPageContent: string) {
+    landingPageContent = removeMarkdownComments(landingPageContent);
+
     const titleMatch = landingPageContent.match(/^#\s*(.*)$/m);
     const title = titleMatch ? titleMatch[1] : null;
 
     const descriptionMatch = landingPageContent.match(/^\s*([^#\s].*)$/m);
-    const description = descriptionMatch ? descriptionMatch[1] : null;
+    const descriptionMarkdown = descriptionMatch ? descriptionMatch[1] : null;
+    const description = removeMarkdownFormatting(descriptionMarkdown);
 
     const imageMatch = landingPageContent.match(/!\[.*\]\((.*)\)/);
     const image = imageMatch ? imageMatch[1] : null;
